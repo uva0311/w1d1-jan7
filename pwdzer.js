d00bc26bf1f2fefb1c2699ba75c0de3dbf4d3fca
Arthur Lam <lam.arthur0311@gmail.com>
2019-01-07 21:13:42 +0000

    Check capital added

--- pwdzer.js
+++ pwdzer.js
@@ -1,7 +1,16 @@
 // i am coding here my password analyzer
 
-//first step is to collect the password from command line
+// first step is to collect the password from command line
 
-var pwd = process.argv
+var pwd = process.argv[2]
 
-console.log('Password to analyze: ', pwd)
+
+// check capital letters
+var allCaptials = "QWERTYUIOPASDFGHJKLZXCVBNM"
+var areThereCapitals = false
+for(var p = 0; p < pwd.length; p++) {
+  if(allCaptials.includes(pwd[p])) { // is a capital letter
+    areThereCapitals = true
+  }
+}
+console.log('Does it have capitals? ', areThereCapitals)
